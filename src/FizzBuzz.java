d7d6717dc8bd6b5d807599d3169ff54b94e203fb
PeterZegarek <petrzegarek@gmail.com>
2024-03-20 19:36:07 -0400

    Solved fizzbuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz{
+    public static void main(String[] args){
+        for(int counter = 0; counter <= 100; counter++){
+            if(counter % 3 == 0 && counter % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if (counter % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if (counter % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(counter);
+            }
+        }
+    }
+}
